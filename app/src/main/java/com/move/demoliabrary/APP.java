4f214dca23513dc07fa3b3f871c3596ffeb5db6c
yqy <yuanqingying@1905.tv>
2017-02-10 10:00:23 +0800

    Initial commit

---
+++ app/src/main/java/com/move/demoliabrary/APP.java
@@ -0,0 +1,15 @@
+package com.move.demoliabrary;
+
+import android.app.Application;
+
+/**
+ * Created by yqy on 10/00102017/2/10/.
+ */
+public class APP extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //设置字体
+    }
+}
